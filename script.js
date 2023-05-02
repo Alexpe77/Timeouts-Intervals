620a9cd1d1471e91e3c4f091568385176d6472d8
Alexpe77 <126241961+Alexpe77@users.noreply.github.com>
2023-05-02 14:58:12 +0200

    Add timer and output exercise2

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const textParagraph = 'Wilson 2';
-    const typeWriter = document.getElementById('typeWriter');
-  
-    let i = 0;
-  
-    const interval = setInterval(() => {
-      typeWriter.innerHTML += textParagraph[i];
-      i++;
-      if (i >= textParagraph.length) {
-        clearInterval(interval);
-        typeWriter.classList.add('visible');
+// Title
+
+const textParagraph = 'Wilson 2';
+const typeWriter = document.getElementById('typeWriter');
+
+let i = 0;
+
+const interval = setInterval(() => {
+  typeWriter.innerHTML += textParagraph[i];
+  i++;
+  if (i >= textParagraph.length) {
+    clearInterval(interval);
+  }
+}, 1000); 
+
+// Timer
+
+let timer = setInterval(countUpTimer, 1000);
+
+let totalSeconds = 0;
+
+function countUpTimer() {
+  totalSeconds++;
+  let hours = Math.floor(totalSeconds / 3600);
+  let minutes = Math.floor((totalSeconds - hours * 3600) / 60);
+  let seconds = totalSeconds - (hours * 3600 + minutes * 60);
+
+  document.getElementById("timer").innerHTML = hours + ":" + minutes + ":" + seconds;
+}
+
+  function displayMinutesPassed() {
+    let minutes = 0;
+    setInterval(function() {
+      minutes++;
+      if (minutes === 1) {
+        document.getElementById('output').innerHTML = "<p>1 minute has passed</p>";
+      } else {
+        document.getElementById('output').innerHTML = `<p>${minutes} minutes have passed</p>`;
       }
-    }, 1000);
-  });  
+    }, 60000);
+  }
+  
+  displayMinutesPassed();
